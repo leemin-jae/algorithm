7203b3b9425536f89289fba757cf35c810b1fa65
이민재 <uiui96@naver.com>
2022-09-04 21:29:52 +0900

    백준 1449 수리공 항송 greedy

--- algo/src/algo/Baek/Main_1449_0904.java
+++ algo/src/algo/Baek/Main_1449_0904.java
@@ -6,45 +6,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_15903_0903 {
-	static int N, M;
-
+public class Main_1449_0904 {
+	static int N, L;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		long Card[] = new long[N];
+		L = Integer.parseInt(st.nextToken());
+		
+		int pipe[] = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			Card[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0 ; i < N ;i ++) {
+			pipe[i] = Integer.parseInt(st.nextToken());
 		}
-
-		
-		for (int tc = 0; tc < M; tc++) {
-			Arrays.sort(Card);
-		
 			
-		
-				long num = Card[0] + Card[1];
-				Card[0] = num;
-				Card[1] = num;
-
-			
-		}
-		
-		long sum = 0;
-		for(int i = 0 ; i <  N ;i ++) {
-			sum += Card[i];
+		Arrays.sort(pipe);
+		int sum = 0;
+		int now = -99999;
+		for (int i = 0; i < pipe.length; i++) {
+			if(now < pipe[i]) {
+				now = pipe[i] + (L-1);
+				sum++;
+			}
 		}
 		
 		System.out.println(sum);
-		
-		
-
+			
 	}
 
 }
