ca923aaf30792ee4d219ab24bd229c6bdf29a6df
이민재 <66997643+leemin-jae@users.noreply.github.com>
2023-01-12 11:06:21 +0900

    [Gold V] Title: 호텔, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -13,45 +13,25 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int dp[] = new int[N+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        int dp[] = new int[N+101];
+        Arrays.fill(dp, 999999999);
+
+        dp[0] = 0;
 
         for(int i = 0 ; i < M ; i++){
             st = new StringTokenizer(br.readLine());
-            int first = Integer.parseInt(st.nextToken());  // 비용
-            int second = Integer.parseInt(st.nextToken());   // 사람 수
+            int cost = Integer.parseInt(st.nextToken());  // 비용
+            int people = Integer.parseInt(st.nextToken());   // 사람 수
 
-            int cnt = 0;
-            int price = second;
-            int people = first;
-            while(true){
-                cnt++;
-                price = second * cnt;
-                people = first * cnt;
-                for(int j = 1 ; j <= price && j <= N ; j++){
-                    dp[j] = Math.min(dp[j], people);
-                    //System.out.println(Arrays.toString(dp));
-                }
-                if(price > N){
-                    break;
-                }
+            for(int j = people ; j < N + 101; j++){
+                dp[j] = Math.min(dp[j], cost + dp[j-people]);
             }
         }
-        //System.out.println(Arrays.toString(dp));
-        for(int i = 2; i <= N; i++){
-            int min = Integer.MAX_VALUE;
-            for(int j = 1 ; j <= i-j ; j++){
-                if(dp[j] != Integer.MAX_VALUE && dp[i-j] != Integer.MAX_VALUE){
-                    min = Math.min(min, dp[j] + dp[i - j]);
-                }
-            }
 
-            dp[i] = Math.min(dp[i],min);
-
+        int result = Integer.MAX_VALUE;
+        for(int i= N ; i < N+101; i++){
+            result = Math.min(result, dp[i]);
         }
-
-        System.out.println(dp[N]);
-
-
+        System.out.println(result);
     }
 }
