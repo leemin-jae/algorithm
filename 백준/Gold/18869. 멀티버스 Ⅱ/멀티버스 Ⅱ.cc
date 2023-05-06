b0785d22f9d7d6726ab7e14214a2cf22507cf04a
이민재 <66997643+leemin-jae@users.noreply.github.com>
2023-05-06 21:26:30 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 208 ms, Memory: 9920 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
@@ -1,21 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-vector<vector<int>> u(101, vector<int>(0));
+#define size first
+#define idx second
  
-void compression(vector<int>& v){   // 벡터에 대해 좌표 압축을 수행하는 함수
-    vector<int> tmp(v.size()); 
-    copy(v.begin(), v.end(), tmp.begin());  // v를 tmp 벡터에 copy
- 
-    sort(tmp.begin(), tmp.end());   // tmp 벡터를 오름차순 정렬
-    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());   // 중복 제거
- 
-    for(int i = 0; i < v.size(); i++){  // X'i 계산
-        int idx = lower_bound(tmp.begin(), tmp.end(), v[i]) - tmp.begin();  
-        v[i] = idx;
-    }
-}
- 
+pair<int, int> u[101][10010];
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -24,22 +13,32 @@ int main(){
     for(int i = 0; i < m; i++){
         for(int j = 0, size; j < n; j++){
             cin >> size;
-            u[i].push_back(size);
+            u[i][j] = {size, j};    // {행성의 크기, 정렬 전 인덱스}
         }
-        compression(u[i]);  // 각각의 우주에 대해 좌표 압축 수행
+        sort(&u[i][0], &u[i][n]);   // 각 우주를 행성 크기를 기준으로 정렬
     }
  
-    int ans = 0;
+    int ans = 0;    // 균등한 우주 쌍의 개수
+ 
     for(int i = 0; i < m - 1; i++){
-        for(int j = i + 1; j < m; j++){ // 두 우주가 균등한지 확인 (Combination)
-            if(u[i].size() == u[j].size()){ // 좌표 압축 후, 좌표의 개수가 같을 때만 균등할 수 있음
-                bool equal = true;  // 두 우주가 균등한지를 나타내는 Flag 변수
-                for(int k = 0; k < u[i].size(); k++){   // 각각의 좌표 압축 결과를 비교
-                    if(u[i][k] != u[j][k]) equal = false;   // 결과가 다르다면, 두 우주가 균등하지 않은 것
+        for(int j = i + 1; j < m; j++){ // 두 우주를 선택 (Combination)
+            bool equal = true;  // 두 우주가 균등한지 나타내는 Flag 변수
+            for(int k = 0; k < n; k++){
+                if(u[i][k].idx != u[j][k].idx) {    // 정렬 전 위치가 같지 않다면
+                    equal = false;  // 두 우주는 균등하지 않은 우주
+                    break;
+                }else{  // 정렬 전 위치가 같다면
+                    if(i != n - 1){
+                        if(u[i][k].size < u[i][k + 1].size != u[j][k].size < u[j][k + 1].size){ // 대소관계 비교
+                            equal = false;
+                            break;
+                        }
+                    }
                 }
-                if(equal) ans++;
+ 
             }
+            if(equal) ans++;
         }
     }
-    cout << ans;
+    cout << ans << "\n";
 }
