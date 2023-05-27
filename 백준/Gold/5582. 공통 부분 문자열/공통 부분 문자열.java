847b4cc35dbc3ae8a55463b286a1608d6569894e
이민재 <66997643+leemin-jae@users.noreply.github.com>
2023-05-27 20:02:34 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 240 ms, Memory: 77032 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -1,68 +1,21 @@
-import org.w3c.dom.Node;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.math.BigInteger;
-import java.util.*;
-
+import java.io.*;
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-
-    public static void main(String[] args) throws IOException {
-
-        String first = br.readLine();
-        String second = br.readLine();
-        int N = 0;
-        String std = null;
-        String cp = null;
-
-        if(first.length() < second.length()){
-            std = first;
-            cp = second;
-            N = first.length();
-        }else{
-            std = second;
-            cp = first;
-            N = second.length();
-        }
-
-        int Map[] = new int [N];
-
-        for(int i = 0 ; i < N; i++){
-            if(cp.contains(std.substring(i , i+1))){
-                Map[i] = 1;
-            }
-        }
-
-//        System.out.println(Arrays.toString(Map));
-//
-        int max = 0;
-        for(int i = 1 ; i < N ; i++){   // 길이
-
-            if(Map[i] != 0 ){
-                    int start = i - Map[i-1];
-                    int end = i+1;
-                    for(int j = start  ; j < end ; j++){
-                        if(cp.contains(std.substring(j , end))){
-//                            System.out.println(std.substring(j , end));
-                            Map[i] = end - j;
-                            break;
-                        }
-
-                }
-                    max = Math.max(max, Map[i]);
-            }
-
-        }
-
-        System.out.println(max);
-
-    }
-
-
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String cmp1 = br.readLine();
+		String cmp2 = br.readLine();
+		int len1 = cmp1.length();
+		int len2 = cmp2.length();
+		int max = 0;
+		int[][] map = new int[len1 + 1][len2 + 1];
+		for(int i = 1; i <= len1; i++) {
+			for(int j = 1; j <= len2; j++) {
+				if(cmp1.charAt(i-1) == cmp2.charAt(j-1)) {
+					map[i][j] = map[i-1][j-1] + 1;
+					max = Math.max(max, map[i][j]);
+				}
+			}
+		}
+		System.out.println(max);
+	}
 }
