1bd208dfc8888348f93773ea03a501b84b75fc97
이민재 <66997643+leemin-jae@users.noreply.github.com>
2023-11-02 18:08:10 +0900

    [Gold V] Title: 자두나무, Time: 80 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,76 +1,49 @@
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PipedReader;
 import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static int N , Min, Max, M , K;
+    static int T , W;
 
-    static int delta[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-
-    static int Map[];
-    static int check[][];
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
 
-        int Dp[][][] = new int[N][2][M + 1];
+        int DP[][][] = new int[3][T][W+1];
+        int Map[] = new int[T];
 
-        if (Integer.parseInt(br.readLine()) == 1) {
-            Dp[0][0][0] = 1;
-        } else {
-            Dp[0][1][1] = 1;       // 자두가 1번 위치에 있기 때문에 무조건 이동해야됨
+        for (int i = 0; i < T; i++) {
+            Map[i] = Integer.parseInt(br.readLine());
+        }
+        if (Map[0] == 1) {
+            DP[Map[0]][0][0] = 1;
+        }else{
+            DP[2][0][1] = 1;
         }
 
-        for (int i = 1; i < N; i++) {
-
-
-            int first = 0;
-            int second = 0;
-            if(Integer.parseInt(br.readLine()) == 1){
-                first = 1;
-            }else{
-                second = 1;
-            }
-            for(int j = 0 ; j <= M ; j++){
-                if(j == 0){                 // 그 자리에서 절대 이동 안할때 
-                    Dp[i][0][j] = Dp[i-1][0][j] + first;
-                    Dp[i][1][j] = Dp[i-1][1][j] + second;
-                }else {                  
-                    Dp[i][0][j] = Math.max(Dp[i - 1][1][j - 1] + first, Dp[i - 1][0][j] + first);    // 이동을 하거나  그자리에서 계속 있을 때 
-                    Dp[i][1][j] = Math.max(Dp[i - 1][0][j - 1] + second, Dp[i - 1][1][j] + second);
+        for (int i = 1; i < T; i++) {
+            int ano = Map[i] == 1? 2 : 1;
+            for (int j = 0; j <= W; j++) {
+                if(j == 0){
+                    DP[Map[i]][i][j] = DP[Map[i]][i-1][j] + 1;
+                    DP[ano][i][j] = DP[ano][i-1][j];
+                }else{
+                    DP[Map[i]][i][j] = Math.max(DP[ano][i-1][j-1] , DP[Map[i]][i-1][j]) + 1;
+                    DP[ano][i][j] = Math.max(DP[Map[i]][i-1][j-1] , DP[ano][i-1][j]);
                 }
             }
-
         }
-        int result = Integer.MIN_VALUE;
-//        for(int i = 0 ; i <= M ; i++){
-//            for(int j = 0 ; j < N ; j++){
-//                System.out.print(Dp[j][0][i] + " ");
-//            }
-//            System.out.println();
-//        }        System.out.println("-----------");
-//
-//        for(int i = 0 ; i <= M ; i++){
-//            for(int j = 0 ; j < N ; j++){
-//                System.out.print(Dp[j][1][i] + " ");
-//            }
-//            System.out.println();
-//        }
-
-        for(int i = 0 ; i <= M ; i++){
-//            System.out.println(Dp[N-1][0][i] + " "  + Dp[N-1][1][i]);
-            result = Math.max(Dp[N-1][0][i] , result);
-            result = Math.max(Dp[N-1][1][i] , result);
+        int result = 0;
+        for (int i = 0; i <= W; i++) {
+            result = Math.max(result,Math.max(DP[2][T-1][i] , DP[1][T-1][i]));
         }
-
         System.out.println(result);
-
     }
-
 }
