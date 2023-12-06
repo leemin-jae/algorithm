02e369988f108ca46c03d75a200012817ad592dd
이민재 <66997643+leemin-jae@users.noreply.github.com>
2023-12-06 14:53:04 +0900

    [Gold IV] Title: 여행 가자, Time: 132 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,52 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
+
+    static int Map[];
     public static void main(String[] args) throws IOException {
         int N  = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
 
-        int[][] Map = new int[N+1][N+1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <=N; j++) {
-                Map[i][j] = Integer.parseInt(st.nextToken());
-            }
+        Map = new int[N+1];
+        for (int i = 0; i <= N; i++) {
+            Map[i] = i;
         }
 
-        int[] area = new int[N+1];
-        boolean[] visited = new boolean[N+1];
         for (int i = 1; i <= N ; i++) {
-            if(!visited[i]) {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.add(i);
-                visited[i] = true;
-                area[i] = i;
-                while (!queue.isEmpty()){
-                    int now = queue.poll();
-                    for (int j = 1; j <= N ; j++) {
-                        if(Map[now][j] == 1 && !visited[j]){
-                            queue.add(j);
-                            visited[j] = true;
-                            area[j] = i;
-                        }
-                    }
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                if(Integer.parseInt(st.nextToken()) == 1){
+                    union(i , j);
                 }
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        int std = area[Integer.parseInt(st.nextToken())];
+        int std = find(Integer.parseInt(st.nextToken()));
 
         while (st.hasMoreTokens()){
-            if(std != area[Integer.parseInt(st.nextToken())]){
+            if(std != find(Integer.parseInt(st.nextToken()))){
                 System.out.println("NO");
                 return;
             }
@@ -55,5 +40,18 @@ public class Main {
         System.out.println("YES");
 
 
+
+    }
+    public static boolean union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a == b) return false;
+        if (a <= b) Map[b]= a;
+        else Map[a] = b;
+        return true;
+    }
+    public static int find(int a){
+        if (Map[a] == a) return a;
+        else return Map[a] = find(Map[a]);
     }
 }
