53eac8aeea6e34b48b94f48e8c601d5c1418ba2f
이민재 <66997643+leemin-jae@users.noreply.github.com>
2024-01-02 17:10:25 +0900

    [level 3] Title: 불량 사용자, Time: 5.88 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,44 +1,22 @@
 import java.util.*;
 class Solution {
-    static Set<Integer> set = new HashSet<>();
+    Set<Integer> set = new HashSet<>();
     public int solution(String[] user_id, String[] banned_id) {
-         int answer = 0;
-        boolean checked[][] = new boolean[user_id.length][banned_id.length];
-
-        for (int i = 0; i < user_id.length; i++) {
-            for (int j = 0; j < banned_id.length; j++) {
-                if (user_id[i].length() != banned_id[j].length()) continue;
-                checked[i][j] = true;
-                for (int k = 0; k < user_id[i].length(); k++) {
-                    if(banned_id[j].charAt(k) == '*') continue;
-                    if(user_id[i].charAt(k) != banned_id[j].charAt(k)){
-                        checked[i][j] = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-
-//        for (int i = 0; i < user_id.length; i++) {
-//            for (int j = 0; j < banned_id.length; j++) {
-//                System.out.print(checked[i][j]?"1" : "0");
-//            }
-//            System.out.println();
-//        }
-
-        cul(0 , 0 , checked);
+        int answer = 0;
+        cul(0 , 0, user_id , banned_id);
         answer = set.size();
         return answer;
     }
-    public void cul(int cnt ,int isUsed , boolean checked[][]){
-        if(cnt == checked[0].length){
+    public void cul(int cnt ,int isUsed , String[] user_id, String[] banned_id){  // 정규식을 이용한 풀이
+        if(cnt == banned_id.length){
             set.add(isUsed);
             return;
         }
-        for (int i = 0; i < checked.length; i++) {
-            if((isUsed & (1 << i)) == 0 && checked[i][cnt]){
-                cul(cnt + 1 , (isUsed | (1 << i)) , checked);
+
+        String reg = banned_id[cnt].replace("*", "[\\w\\d]");
+        for (int i = 0; i < user_id.length; i++) {
+            if((isUsed & (1 << i)) == 0 && user_id[i].matches(reg)){
+                cul(cnt + 1 , (isUsed | (1 << i)) , user_id , banned_id);
             }
         }
 
