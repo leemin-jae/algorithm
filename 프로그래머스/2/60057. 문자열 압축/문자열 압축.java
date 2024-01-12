b3bce07126d8d6e5511e0aaabf6a65a4d6bbd0f8
이민재 <66997643+leemin-jae@users.noreply.github.com>
2024-01-12 04:33:39 -0800

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,42 +1,33 @@
 class Solution {
     public int solution(String s) {
-        int answer = 10000;
-        for(int i = 1 ; i <= s.length() ;i++){
-          
-            
-            StringBuilder sb = new StringBuilder();
-            String check = s.substring(0 , i);
-            int num = 1;
-            int j = i;
-            for(; j <= s.length() - i ; j+=i){
-                String next = s.substring(j , j+i);
-                if(check.equals(next)){
-                    num++;
-                }else{
-                    if(num == 1){
-                        sb.append(check);
-                    }else{
-                        sb.append(num).append(check);
+        int min = s.length();
+        int len = s.length()/2+1;
+        for(int i = 1; i < len; i++) {
+            String before = "";
+            int sum = 0;
+            int cnt = 1;
+            for(int j = 0; j < s.length();) {               
+                int start = j;
+                j = (j+i > s.length()) ? s.length():j+i;
+                String temp = s.substring(start, j);
+                if(temp.equals(before)) {
+                    cnt++;
+                } else {
+                    if(cnt != 1) {
+                        sum += (int)Math.log10(cnt)+1;
                     }
-                    check = next;
-                    num = 1;
+                    cnt = 1;
+                    sum+=before.length();
+                    before = temp;
                 }
-
             }
-            if(num == 1){
-                sb.append(check);
-            }else{
-                sb.append(num).append(check);
+            sum+=before.length();
+            if(cnt != 1) {
+                sum += (int)Math.log10(cnt)+1;
             }
-            
-            
-            sb.append(s.substring(j));
-            
-            answer = Math.min(answer , sb.toString().length());
-            
-            
+            min = (min > sum) ? sum : min;
         }
-        
-        return answer;
+
+        return min;
     }
 }
