60ac3a5cb887bb638fde954c486b856f7d0acca6
이민재 <66997643+leemin-jae@users.noreply.github.com>
2024-01-15 17:00:49 -0800

    [level 3] Title: 단속카메라, Time: 5.99 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    static class Node implements Comparable<Node>{
+        int start;
+        int end;
+        Node(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+        
+        @Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return this.start - o.start;
+		}
+    }
+    public int solution(int[][] routes) {
+        List<Node> list = new ArrayList<>();
+        for(int i = 0 ; i < routes.length ; i++){
+            list.add(new Node(routes[i][0], routes[i][1]));
+        }
+        
+        Collections.sort(list);
+        Node n = list.get(0);
+        int s = n.start;
+        int e = n.end;
+        int sum = 1;
+        for(int i = 1 ; i < list.size(); i++){
+            Node now = list.get(i);
+            if(now.start <= e){
+                e = now.end < e? now.end:e;
+            }else{
+                sum++;
+                e = now.end;
+            }
+        }
+        return sum;
+    }
+}
