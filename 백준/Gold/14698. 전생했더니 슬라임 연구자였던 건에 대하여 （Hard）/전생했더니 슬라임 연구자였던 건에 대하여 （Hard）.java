5225a908aca888a9717afba398d8bbfbb3910e3b
이민재 <66997643+leemin-jae@users.noreply.github.com>
2024-02-19 00:23:53 -0800

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 1148 ms, Memory: 165068 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int T = 0; T < tc; T++) {
+            int N = Integer.parseInt(br.readLine());
+
+            PriorityQueue<Long> queue = new PriorityQueue<>();
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                queue.add(Long.parseLong(st.nextToken()));
+            }
+            long answer = 1;
+            while (queue.size() != 1){
+                long min = queue.poll() * queue.poll();
+                answer = answer * (min % 1000000007 ) % 1000000007;
+                queue.add(min);
+            }
+            sb.append(answer).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}
